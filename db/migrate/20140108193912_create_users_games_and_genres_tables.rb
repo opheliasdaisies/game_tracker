6a4b7db407cdbb0def30b3fa9f3fa82ca9706339
Sara Gorecki <sgorecki@gmail.com>
2014-01-08 14:45:19 -0500

    create tables for users, games, and genres and connect tables

---
+++ db/migrate/20140108193912_create_users_games_and_genres_tables.rb
@@ -0,0 +1,34 @@
+class CreateUsersGamesAndGenresTables < ActiveRecord::Migration
+  def create
+    create_table :users do |t|
+      t.string :username
+      t.string :password
+      t.string :email
+      t.text :bio
+      t.timestamps
+    end
+
+    create_table :games do |t|
+      t.string :title
+      t.integer :rating
+      t.text :description
+      t.timestamps
+    end
+
+    create_table :genres do |t|
+      t.string :name
+      t.text :bio
+      t.timestamps
+    end
+
+    create_table :users_games do |t|
+      t.belongs_to :user
+      t.belongs_to :game
+    end
+
+    create_table :games_genres do |t|
+      t.belongs_to :game
+      t.belongs_to :genre
+    end
+  end
+end
