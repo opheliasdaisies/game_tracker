db2e0a2afb8515f3e74307718e3a3d5da0d53835
Sara Gorecki <sgorecki@gmail.com>
2014-01-08 14:17:35 -0500

    add initial rails files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GameTracker::Application.config.secret_token = '86aeb74332b787a9861d5dac163a744f1b6908db02e8ee1c6e8a94ca9c75b3d63d90c5109e9a6024c7e3ca29630ebc3a5637c3d0f3f5ae5aebec8e39eebd06aa'
